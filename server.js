15fdace06b83b65448cbf6a1c23572775c5365ca
rekalo.alex <rekalo.alex@hotmail.com>
2020-09-23 23:11:08 +0300

    static path fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('./dist/e-commer-angular9'));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile('index.html', {root: 'dist/e-commer-angular9/'});
 });
 
